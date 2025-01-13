990942711c907d554c58b0482f4d69fb696e443a
Your Name <you@example.com>
2025-01-13 11:50:08 +0530

    chnges:roshan

---
+++ curd.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
+<body>
+    <form action="savedata.php" method="POST">
+        <div class="company-section">
+        <label>Company</label>
+        <br>
+        <input type="text" name="company" placeholder="Enter Company Name ">
+        </div>
+        <div class="contact-section">
+            <label>Contact</label>
+            <br>
+            <input type="number" name="contact" placeholder="Enter your contact no" />
+        </div>
+        <div class="country-section">
+            <label>Country</label>
+            <br>
+            <select name="country">
+                <option value="india">india</option>
+                <option value="Australia">Australia</option>
+                <option value="nepal">nepal</option>
+            </select>
+        </div>
+        <div class="gender-section">
+          <label>Gender</label>
+        <input type="radio" value="Male">Male
+        <input type="radio" value="Female">Female
+        </div>
+        <div class="favourite_subject">
+          <label>Favourite Subject</label>
+          <input type="checkbox" value="Maths">Maths
+          <input type="checkbox" value="Science">Science
+         </div>
+        <br>
+        <div class="button">            
+           <input type="submit">
+        </div>
+    </form>
+
+<h2>A basic HTML table</h2>
+
+<table style="width:100%">
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+
+<p>To understand the example better, we have added borders to the table.</p>
+
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+ echo "hello";
+
+ $data = "roshan";
+
+ echo "<br>".$data;
+
+
+ $new = 4;
+
+ echo "<br>".$new;
+
+ print "<br>".$new;
+
+echo "<br>";
+ $cars = array("Volvo","BMW",40000069);
+
+ print_r($cars);
+//var_dump($cars);
+?>
+
+
+<?="<br>"."fsdfsf" ?>
